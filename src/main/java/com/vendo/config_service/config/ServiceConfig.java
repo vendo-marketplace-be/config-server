9eeb84bc5ad2e306c269c5524eba249d0660fa70
HaaaamBurger <gemasclashes@gmail.com>
2025-07-19 14:47:17 +0300

    first commit

---
+++ src/main/java/com/vendo/config_service/config/ServiceConfig.java
@@ -0,0 +1,9 @@
+package com.vendo.config_service.config;
+
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigServer
+public class ServiceConfig {
+}
